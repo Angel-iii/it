85d3f94fa06d0daf077df0ad9dc022977a175250
Angel-iii <angelcity0001@gmail.com>
2024-08-03 03:07:15 +0500

    d

--- main8.js
+++
@@ -1,5 +0,0 @@
-let character = "Hello";
-console.log(character); // This will print "Hello" to the console
-
-character = "World"; // Reassigning the value of character
-console.log(character); // This will print "World" to the console
